d9c9db74ba64ace9ffb43f26bf55f6e26a40b31b
DAppArtisan <dexterdigitaltechnologies@gmail.com>
2023-11-09 18:55:27 +0530

    updatea api

--- pages/api/mailsend.js
+++ pages/api/mailsend.js
@@ -1,29 +1,29 @@
 
-const mail = require('@sendgrid/mail');
+// const mail = require('@sendgrid/mail');
 
-mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+// mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
-export default async (req, res) => {
-  const body = JSON.parse(req.body);
+// export default async (req, res) => {
+//   const body = JSON.parse(req.body);
 
-  const message = `
-    Name: ${body.name}\r\n
-    Email: ${body.email}\r\n
-    Message: ${body.message}
-  `;
+//   const message = `
+//     Name: ${body.name}\r\n
+//     Email: ${body.email}\r\n
+//     Message: ${body.message}
+//   `;
 
-  try{
-    await mail.send({
-      to: 'info@dexterinc.com',
-      from: 'info@dexterinc.com',
-      subject: 'New Query from Dexter Digital Technologies',
-      text: message,
-      html: message.replace(/\r\n/g, '<br>'),
-    });
-    res.status(200).json({ status: 'Ok' });
-  } catch(error){
-    res.status(400).json({ status: "ERROR", message: error.message });
-  }
+//   try{
+//     await mail.send({
+//       to: 'info@dexterinc.com',
+//       from: 'info@dexterinc.com',
+//       subject: 'New Query from Dexter Digital Technologies',
+//       text: message,
+//       html: message.replace(/\r\n/g, '<br>'),
+//     });
+//     res.status(200).json({ status: 'Ok' });
+//   } catch(error){
+//     res.status(400).json({ status: "ERROR", message: error.message });
+//   }
 
 
-}
+// }
